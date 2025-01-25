e3e6aa79e44262b39424b76e2f5b915d61a3c84a
rizchi17 <rizchi17ub@gmail.com>
2025-01-25 18:35:07 +0900

    mainの整備

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_runzonedguarded/runzoned.dart';
+import 'package:flutter_runzonedguarded/zone.dart';
 
 Future<void> main() async {
   runApp(const MyApp());
@@ -29,37 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<void> synchronous() async {
-    try {
-      throw Exception('エラー発生');
-    } catch (error, _) {
-      print('ここでキャッチできる');
-      print('捕捉したエラー: $error');
-    }
-  }
-
-  Future<void> asynchronous() async {
-    try {
-      Future<void>.delayed(Duration.zero).then((_) {
-        throw Exception('エラー発生');
-      });
-    } catch (error, _) {
-      print('ここでキャッチできない');
-      print('捕捉したエラー: $error');
-    }
-  }
-
-  Future<void> asynchronousWithRunZonedGuarded() async {
-    runZonedGuarded(() {
-      Future<void>.delayed(Duration.zero).then((_) {
-        throw Exception('エラー発生');
-      });
-    }, (error, stackTrace) {
-      print('ここでキャッチできる');
-      print('捕捉したエラー: $error');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,32 +44,14 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             ElevatedButton(
                 onPressed: () {
-                  synchronous();
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ZonePage(title: 'ゾーンを理解')));
                 },
-                child: Text('同期的 + tryCatch')),
-            SizedBox(height: 50),
+                child: Text('ゾーンを理解')),
             ElevatedButton(
                 onPressed: () {
-                  asynchronous();
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => RunZonedPage(title: 'runZonedを理解')));
                 },
-                child: Text('非同期的 + tryCatch')),
-            SizedBox(height: 50),
-            ElevatedButton(
-                onPressed: () async {
-                  await runZonedGuarded(() async {
-                    asynchronous();
-                  }, (error, stackTrace) async {
-                    print('ここでキャッチできる');
-                    print(error);
-                  });
-                },
-                child: Text('非同期的 + tryCatch + runZonedGuarded')),
-            SizedBox(height: 50),
-            ElevatedButton(
-                onPressed: () {
-                  asynchronousWithRunZonedGuarded();
-                },
-                child: Text('非同期的 + runZonedGuarded')),
+                child: Text('runZonedを理解')),
           ],
         ),
       ),
